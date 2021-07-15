a8a13afcbfb16eff070f1ae4dc604309fcc016a0
Vishnu Haridas <vishnu.haridas@fingent.com>
2021-07-15 11:38:54 +0530

    Added main test

---
+++ test/main_test.js
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+// Test suite
+describe("Mocha", () => {
+  // Test spec (unit test)
+  it("should run our tests using npm", () => {
+    expect(true).to.be.ok;
+  });
+});
